058d1e46da3235f2c162e552f46011a6d524586b
Vijay Gupta <vijayg963@gmail.com>
2022-03-20 00:37:19 +0530

    Assignment-1

---
+++ block-BNaacn/code/modal/users.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, lowercase: true },
+  age: { type: Number, default: 0 },
+  favorites: [String],
+  marks: [Number],
+  createdAt: { type: Date, default: new Date() },
+  pasword: { minlength: 5, maxlength: 15 },
+  address: {
+    village: String,
+    city: String,
+    state: String,
+    pin: number,
+    user: Schema.Types.ObjectId,
+  },
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
