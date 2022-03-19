193aef02731fbeab03bf4d268b9558e8f4d613d8
Vijay Gupta <vijayg963@gmail.com>
2022-03-20 00:49:47 +0530

    Assignment-1

--- block-BNaacn/code/modal/fields.js
+++ block-BNaacn/code/modal/fields.js
@@ -1,13 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var fieldSchema = new Schema({
-  village: String,
-  city: String,
-  state: String,
-  pin: number,
-  user: Schema.Types.ObjectId,
-});
+var fieldSchema = new Schema(
+  {
+    village: String,
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pin: number,
+    user: Schema.Types.ObjectId,
+    address: {},
+  },
+  { timestamps: true }
+);
 
 var Fields = mongoose.model('Fields', fieldSchema);
 
