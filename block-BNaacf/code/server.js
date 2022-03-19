3d1ae780234a69245578f5ed52791db4996e659e
Vijay Gupta <vijayg963@gmail.com>
2022-03-19 22:05:45 +0530

    array-obj-and objectId

---
+++ block-BNaacf/code/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+// connect to the mongoose
+mongoose.connect(
+  'mongodb://localhost/code',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  }
+);
+
+// Applications
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.listen(4000, () => {
+  console.log('Port is listing for 4k');
+});
